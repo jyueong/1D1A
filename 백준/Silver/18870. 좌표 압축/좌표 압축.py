141078efb05ea04d7c96ad90f220aad6fc25e4d0
jyueong <109324439+jyueong@users.noreply.github.com>
2022-09-09 21:23:58 +0900

    [Silver II] Title: 좌표 압축, Time: 2340 ms, Memory: 179272 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,12 @@
+N = int(input())
+lst = list(map(int, input().split()))
+lst_s = sorted(list(set(lst)))
+dict_lst = {}
+
+for i in range(len(lst_s)):
+    dict_lst[f'{lst_s[i]}'] = i
+
+
+for i in range(len(lst)):
+    lst[i] = dict_lst.get(f'{lst[i]}')
+print(*lst)
