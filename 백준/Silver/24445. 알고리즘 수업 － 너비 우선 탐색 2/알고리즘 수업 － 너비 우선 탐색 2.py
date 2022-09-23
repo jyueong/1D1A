e4dbf9f75e827f1ef79c5b9e8dd65e7e02383643
jyueong <109324439+jyueong@users.noreply.github.com>
2022-09-23 14:07:35 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 764 ms, Memory: 58004 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+
+def bfs(e, r):
+    global cnt
+    q.append(r)
+    visited[r] = cnt
+    while q:
+        v = q.popleft()
+        for node in e[v]:
+            if visited[node] == 0:
+                cnt += 1
+                visited[node] = cnt
+                q.append(node)
+
+
+n, m, r = map(int, sys.stdin.readline().rstrip().split())
+nodes = list([] for _ in range(n+1))
+
+for _ in range(m):
+    u, v = map(int, sys.stdin.readline().rstrip().split())
+    nodes[u].append(v)
+    nodes[v].append(u)
+
+for i in range(n+1):
+    nodes[i].sort(reverse=True)
+
+q = deque()
+visited = [0]*(n+1)
+cnt = 1
+
+bfs(nodes, r)
+
+for i in range(1, n+1):
+    print(visited[i])
