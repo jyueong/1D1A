54638ae00893907a8b10795a863434edc1c97180
jyueong <109324439+jyueong@users.noreply.github.com>
2024-05-14 21:28:15 +0900

    [Silver II] Title: DFS와 BFS, Time: 76 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -4,42 +4,42 @@ input = sys.stdin.readline
 
 n, m, v = map(int, input().split())
 
-graph = [[0]*(n+1) for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
 
 for i in range(m):
     x, y = map(int, input().split())
-    graph[x][y] = 1
-    graph[y][x] = 1
+    graph[x].append(y)
+    graph[y].append(x)
 
-visited = [0]*(n+1)
-ans = list()
+visited = [False]*(n+1)
+dfs_ans = list()
 
 def dfs(v):
-    visited[v] = 1
-    ans.append(v)
-    for i in range(1, n+1):
-        if visited[i] == 0 and graph[v][i] == 1:
+    visited[v] = True
+    dfs_ans.append(v)
+    for i in sorted(graph[v]):
+        if not visited[i]:
             dfs(i)
 
 dfs(v)
 
-print(*ans)
+print(*dfs_ans)
 
-visited2 = [0]*(n+1)
-ans2 = list()
+visited = [False]*(n+1)
+bfs_ans = list()
 
 def bfs(v):
-    q = deque()
-    q.append(v)
-    visited2[v] = 1
-    while q:
-        v = q.popleft()
-        ans2.append(v)
-        for i in range(1, n+1):
-            if visited2[i] == 0 and graph[v][i] == 1:
-                q.append(i)
-                visited2[i] = 1
+    dq = deque()
+    dq.append(v)
+    visited[v] = True
+    while dq:
+        v = dq.popleft()
+        bfs_ans.append(v)
+        for i in sorted(graph[v]):
+            if not visited[i]:
+                dq.append(i)
+                visited[i] = 1
 
 bfs(v)
 
-print(*ans2)
+print(*bfs_ans)
